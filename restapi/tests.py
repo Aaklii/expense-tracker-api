9893f7a891a17215182671d286cc2c3b2e13de4f
Aaklii <solankish66@gmail.com>
2020-11-19 07:11:42 +0530

    Added model for expenses. Added tests for it.

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from restapi import models
+
 
 # Create your tests here.
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=200, merchant="amazon", description="headphones", category="music"
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+        self.assertEqual(200, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
